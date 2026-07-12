4a295877e3ca490fbe4ccc976e106c61957a3305
coolking70 <aannvvc@gmail.com>
2026-07-12 18:08:54 +0800

    test: include v0.7.1 campaign suite

--- scripts/campaign-test.ts
+++ scripts/campaign-test.ts
@@ -1,6 +1,7 @@
 import { runCampaignTests } from '../src/campaign/campaignTests';
 import { runV07Tests } from '../src/campaign/v07Tests';
+import { runV071Tests } from '../src/campaign/v071Tests';
 
-const suites = [runCampaignTests(), runV07Tests()];
+const suites = [runCampaignTests(), runV07Tests(), runV071Tests()];
 for (const suite of suites) console.log(suite.messages.join('\n'));
 (globalThis as any).process.exit(suites.every((suite) => suite.passed) ? 0 : 1);
