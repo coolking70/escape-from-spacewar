d6f9ad26c47eb62350ce14d3a4913862c8b95334
coolking70 <aannvvc@gmail.com>
2026-07-13 02:50:53 +0800

    style: load V0.9 organization UI

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css';
 import './v071.css';
 import './v081.css';
+import './v09.css';
 import { App } from './App';
 import { installCampaignRuntimeControls } from './campaign/fleet/campaignRuntimeControls';
 import { runDeterministicTest, runBalanceTest } from './sim/deterministicTest';
